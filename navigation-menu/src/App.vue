8e02d38984bca1528c7ef044072d2f11f52224a3
zhahua <zhahua@microsoft.com>
2022-05-07 08:41:21 -0700

    navigation-menu by vue.js 3.0

---
+++ navigation-menu/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app">
+    <Navigation/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+export default {
+  components: {
+     Navigation
+  },
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?famliy-Raleway:gth@400;500&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Raleway", sans-serif;
+  font-weight: 400;
+}
+
+.app {
+  min-height: 100vh;
+  position: relative;
+  background-color: #f1f1f1;
+}
+.container {
+  padding: 0 20px;
+  max-width: 1140px;
+  margin: 0 auto;
+}
+</style>
---
+++ navigation-menu/src/components/Navigation.vue
@@ -0,0 +1,95 @@
+<template>
+  <header :class="{ 'scrolled-nav': scrollPosition }">
+    <nav>
+      <div class="branding">
+        <img src="@/assets/logo.png" alt="">
+      </div>
+      <ul v-show="!mobile" class="navigation">
+        <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+        <li><router-link class="link" :to="{name: ''}">About</router-link></li>
+        <li><router-link class="link" :to="{name: ''}">Portfolio</router-link></li>
+        <li><router-link class="link" :to="{name: ''}">Contact</router-link></li>
+      </ul>
+      <div class="icon">
+        <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{'icon-active' : mobileNav}"></i>
+      </div>
+      <transition name="mobile-nav">
+        <ul v-show="mobileNav" class="dropdown-nav">
+          <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+          <li><router-link class="link" :to="{name: ''}">About</router-link></li>
+          <li><router-link class="link" :to="{name: ''}">Portfolio</router-link></li>
+          <li><router-link class="link" :to="{name: ''}">Contact</router-link></li>
+          <li><router-link class="link" :to="{name: ''}">Home</router-link></li>
+        </ul>
+      </transition>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: .5s ease all;
+  color: #fff;
+
+  nav {
+    display: flex;
+    flex-direction: row;
+    padding: 12px 0;
+    transition: .5s ease all;
+    width: 90%;
+    margin: 0 auto;
+    @media (min-width: 1140px) {
+      max-width: 1140px;
+    }
+
+    ul,
+    .link {
+      font-weight: 500;
+      color: #fff;
+      list-style: none;
+      text-decoration: none;
+    }
+
+    li {
+      text-transform: uppercase;
+      padding: 16px;
+      margin-left: 16px;
+
+    }
+
+    .link {
+      font-size: 14px;
+      transition: .5s ease all;
+      padding-bottom: 4px;
+      border-bottom: 1px solid transparent;      
+    }
+
+    &:hover {
+      color: #00afea;
+      border-color: #00afea;
+    }
+
+    .branding {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 50px;
+        transition: .5s ease all;
+      }
+    }
+  }
+
+ 
+
+}
+
+</style>
