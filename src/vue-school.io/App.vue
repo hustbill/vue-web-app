7aeac738be6291f2a2106aa8f08834df4417dc2a
zhahua <zhahua@microsoft.com>
2022-05-15 09:57:00 -0700

    create vue-project using vite, SFC playground

---
+++ src/vue-school.io/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { defineComponent } from '@vue/composition-api'
+import { ref } from '@vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+
+const header =  ref('Shopping List App')
+</script>
+
+<template> 
+    <h1> {{ header}}  </h1>
+</template>
